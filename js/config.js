6a3bc8aa58a61c55ad109bd1ee1e6f3d4837cdaa
ChenghsunLi <91656254+ChenghsunLi729@users.noreply.github.com>
2026-01-04 19:16:27 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -21,9 +21,9 @@ Please credit "0J (Lin Jie / 0rigin1856)" when redistributing or modifying this 
 
 const API_CONFIG = {
   // 正式環境的 API URL
-  apiUrl: "https://script.google.com/macros/s/AKfycbwG_jJl_XWE6SV3hSX8C_uVROmGfWjDoh-QbzVuojnyu7EOcJVKNdp03pezttrIe9E5jA/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwXBYdfq2zjMbVPtW5s0Umskb8nEjQhSoHVRo9s13l5jOFaOA0QSFjLb2gWqEkLq8Fccg/exec",
   // 新增回呼網址
-  redirectUrl: "https://script.google.com/macros/s/AKfycbwG_jJl_XWE6SV3hSX8C_uVROmGfWjDoh-QbzVuojnyu7EOcJVKNdp03pezttrIe9E5jA/exec"
+  redirectUrl: "https://script.google.com/macros/s/AKfycbwXBYdfq2zjMbVPtW5s0Umskb8nEjQhSoHVRo9s13l5jOFaOA0QSFjLb2gWqEkLq8Fccg/exec"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v1.0'
