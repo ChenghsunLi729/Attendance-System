345199e56d9fbd8bfc098519ec787b3cf9c89327
ChenghsunLi <91656254+ChenghsunLi729@users.noreply.github.com>
2026-01-04 18:41:08 +0800

    Update API and redirect URLs in config.js

--- js/config.js
+++ js/config.js
@@ -21,9 +21,9 @@ Please credit "0J (Lin Jie / 0rigin1856)" when redistributing or modifying this 
 
 const API_CONFIG = {
   // 正式環境的 API URL
-  apiUrl: "https://script.google.com/macros/s/AKfycbxAlQjOgCKcMI9oH1ksSDovSzHLPAQmSSCnXSdFAk-ztglDjmxKlkqPJ2u909YMHaYZzA/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzxNuM6VkcmCF_gCJ1dDYIuWIMWxZVBZuSdroANqlJ2IWDbxtJ1TIJKpCRrKvUFLjbyZg/exec",
   // 新增回呼網址
-  redirectUrl: "https://script.google.com/macros/s/AKfycbxAlQjOgCKcMI9oH1ksSDovSzHLPAQmSSCnXSdFAk-ztglDjmxKlkqPJ2u909YMHaYZzA/exec"
+  redirectUrl: "https://script.google.com/macros/s/AKfycbzxNuM6VkcmCF_gCJ1dDYIuWIMWxZVBZuSdroANqlJ2IWDbxtJ1TIJKpCRrKvUFLjbyZg/exec"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v1.0'
