077835bc0ea95de51bbf1213e6f99198f0b1f064
ChenghsunLi <91656254+ChenghsunLi729@users.noreply.github.com>
2026-01-04 23:23:57 +0800

    Update API and redirect URLs in config.js

--- js/config.js
+++ js/config.js
@@ -2,9 +2,9 @@
 
 const API_CONFIG = {
   // 正式環境的 API URL
-  apiUrl: "https://script.google.com/macros/s/AKfycbzvZPwRXNBfbsi9YICkDCu3lfTRjofMth_OwViQzGVGkoba-2yunqlHQluEp3QF_qzh/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbzde3-GDmACYugLkn6cJjLtdZHa-1qjxlI5CYcOyK_FUBH9XRtT5D5iQ9rItRhDW4VJTA/exec",
   // 新增回呼網址
-  redirectUrl: "https://0rigind1865-bit.github.io/Attendance-System/"
+  redirectUrl: "https://chenghsunli729.github.io/Attendance-System/"
   // 你也可以在這裡加入其他設定，例如：
   // timeout: 5000,
   // version: 'v1.0'
